75321f6a2e8bb23e2b43f54bb302af03475c8c04
institutoinfnet <168759697+institutoinfnet@users.noreply.github.com>
2024-08-05 02:58:50 +0000

    Novo exercicio

--- exercicio07.py
+++ exercicio07.py
@@ -1,3 +1,8 @@
 # Exercicio 7
+"""João estava com um parágrafo do capítulo de Dom Casmurro em um arquivo word. Acabou sem querer substituindo todos os 'e' por 3 e todos os 'o' por 0.
+O texto com as substituições está na variável dom_casmurro. Ajude João e escreva um código para escrever na tela o texto original.
+"""
 
+dom_casmurro = """O m3u fim 3vid3nt3 3ra atar as duas p0ntas da vida, 3 r3staurar na v3lhic3 a ad0l3scência. P0is, s3nh0r, nã0 c0ns3gui r3c0mp0r 0 qu3 f0i n3m 0 qu3 fui. Em tud0, s3 0 r0st0 é igual, a fisi0n0mia é dif3r3nt3. S3 só m3 faltass3m 0s 0utr0s, vá; um h0m3m c0ns0la-s3 mais 0u m3n0s das p3ss0as qu3 p3rd3; mas falt0 3u m3sm0, 3 3sta lacuna é tud0. O qu3 aqui 3stá é, mal c0mparand0, s3m3lhant3 à pintura qu3 s3 põ3 na barba 3 n0s cab3l0s, 3 qu3 ap3nas c0ns3rva 0 hábit0 3xt3rn0, c0m0 s3 diz nas autópsias; 0 int3rn0 nã0 agu3nta tinta. Uma c3rtidã0 qu3 m3 d3ss3 vint3 an0s d3 idad3 p0d3ria 3nganar 0s 3stranh0s, c0m0 t0d0s 0s d0cum3nt0s fals0s, mas nã0 a mim. Os amig0s qu3 m3 r3stam sã0 d3 data r3c3nt3; t0d0s 0s antig0s f0ram 3studar a g30l0gia d0s camp0s-sant0s. Quant0 às amigas, algumas datam d3 quinz3 an0s, 0utras, d3 m3n0s, 3 quas3 t0das cr33m na m0cidad3. Duas 0u três fariam cr3r n3la a0s 0utr0s, mas a língua qu3 falam 0briga muita v3z a c0nsultar 0s dici0nári0s, 3 tal fr3quência é cansativa."""
 
+# Escreva seu código abaixo
