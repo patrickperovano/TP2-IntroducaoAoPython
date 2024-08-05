3cb993ffdcc10027964a1c883b74cb3e19cfccea
institutoinfnet <168759697+institutoinfnet@users.noreply.github.com>
2024-08-05 03:37:54 +0000

    Ultimo exercicio

--- exercicio12.py
+++ exercicio12.py
@@ -1,2 +1,12 @@
 # Exercício 12
 
+"""Sobre as chamadas para a função print responda e justifique cada um dos itens.
+
+a) Quantos argumentos estão sendo passados para a função na chamada print("Hello World!")?
+
+b) Quantos argumentos estão sendo passados para a função na chamada print("Hello", "World!")?
+
+c) Quantos argumetos estão sendo passados para a função na chamada
+print("Hello " + "World!" )?
+
+"""
