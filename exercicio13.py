0f28d99e28ce100206fcf59a5805071a735bc12b
institutoinfnet <168759697+institutoinfnet@users.noreply.github.com>
2024-08-14 19:26:48 +0000

    Ajuste na questão 13

--- exercicio13.py
+++ exercicio13.py
@@ -1,7 +1,7 @@
 # Exercício 13
 
 """
-Um número palíndromo é aquele que lido de trás pra frente é a mesma coisa que lendo de trás pra frente.
+Um número palíndromo é aquele que lido de trás pra frente é a mesma coisa que lendo de frente pra trás.
 Por exemplo, o número 121, 1991, 1001 são palídromos.
 Escreva um programa que receba entrada um número inteiro e transforme o mesmo é um número palíndromo juntando ele com o inverso do mesmo. Por exemplo se o usuário der como entrada o número 23 basta juntar 23 com o inverso 32 para formar o número palíndromo 2332. O seu código deve conter comentários para explicar o que está sendo feito.
 
