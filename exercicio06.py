6d116b4e1181cd1e996f83b8f01b0fd1671804f8
Patrick Perovano <53125389+patrickperovano@users.noreply.github.com>
2024-08-17 20:30:02 +0000

    6

--- exercicio06.py
+++ exercicio06.py
@@ -17,14 +17,25 @@ Você deve utilizar código python para ajudar a responder essas perguntas.
 
 palavra = "pneumoultramicroscopicossilicovulcanoconiotico"
 
-# Item a
+# Item a - Quantas letras possui essa palavra?
+letra_a = len(palavra)
+print (letra_a)
 
-# Item b
+# Item b - Quantas vezes a letra 'o' aparece nessa palavra?
+letra_b = palavra.count('o')
+print (letra_b)
 
-# Item c
+# Item c - Qual a posição da letra v nessa palavra?
+letra_c = palavra.find ('v')
+print (letra_c)
 
-# Item d
+# Item d - Escreva essa palavra de trás para frente.
+letra_d = palavra[::-1]
+print (letra_d)
 
-# Item e
-
-
+# Item e - Divida essa palavra em duas com a mesma quantidade de caracteres.
+meio = len(palavra) // 2
+primeira_parte = palavra[:meio]
+segunda_parte = palavra[meio:]
+print (primeira_parte)
+print (segunda_parte)
