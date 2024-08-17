ee762378eca5d69f34e143900bd4ca6a7a168305
Patrick Perovano <53125389+patrickperovano@users.noreply.github.com>
2024-08-17 19:33:30 +0000

    TP2

--- exercicio01.py
+++ exercicio01.py
@@ -7,6 +7,10 @@ a0 = 10
 a1 = "10"
 a2 = 10.0
 
+print (type(a0))
+print (type(a1))
+print (type(a2))
+
 b0 = a0 + a2
 b1 = a2 + a2
 b2 = 5 * a0
@@ -17,23 +21,39 @@ isso_eh_um_int = 10.0
 isso_eh_um_float = "10"
 isso_eh_uma_string = 10
 
+print (type(b0))
+print (type(b1))
+print (type(b2))
+print (type(b3))
+print (type(b4))
+
+print (type(isso_eh_um_int))
+print (type(isso_eh_um_float))
+print (type(isso_eh_uma_string))
+
+
+
+
+
 ###############################################
 #                Perguntas
 ###############################################
 """
 Qual o tipo das variáveis a0, a1 e a2?
 """
-# Resposta
+# Resposta: a0 = int, a1 = string, a2 = float
 """
 Qual o tipo das váriaveis b0, b1, b2, b3 e b4? 
 """
-# Resposta
+# Resposta: b0 = float, b1 = float, b2 = int, b3 = int, b4 = float
 """
 Qual o tipo das variaveis isso_eh_um_int, isso_eh_um_float e isso_eh_uma_string?
 (Note que o nome da variável não influencia no seu tipo)
 """
-# Resposta
+# Resposta: isso_eh_um_int = float, isso_eh_um_float = str, isso_eh_uma_string = int 
 """
 Caso existisse um código entrada_do_usuario = input(), qual seria o tipo da variável entrada_do_usuario? O que deveria ser feito caso você queira utilizar a entrada do usuário como um número inteiro?
 """
-# Resposta
+# Resposta: seria do tipo string. Entretanto, para converter a entrada de str para int, basta utilizar o codigo/expressao a seguir:
+#entrada_do_usuario = input("Digite um número: ")
+#numero_int = int(entrada_do_usuario)
--- exercicio02.py
+++ exercicio02.py
@@ -16,23 +16,30 @@ Dica: Lembre-se de que em Python, os índices das strings começam em 0.
 
 mensagem = "Instituto Infnet"
 
-# Item a
+# Item a - Imprima o primeiro caractere da string. 
 # Escreva seu código abaixo
+print (mensagem[0])
 
-# Item b
+# Item b - Imprima o último caractere.
 # Escreva seu código abaixo
+print (mensagem[-1])
 
-# Item c
+# Item c - Imprima o último caractere de maneira diferente do item b.
 # Escreva seu código abaixo
+print (mensagem[len(mensagem) -1])
 
-# Item d
+# Item d - Imprima o terceiro caractere da string.
 # Escreva seu código abaixo
+print (mensagem[2])
 
-# Item e
+# Item e - Imprima os três primeiros caracteres da string.
 # Escreva seu código abaixo
+print (mensagem[0:3])
 
-# Item f
+# Item f - Imprima os dois últimos caracteres da string.
 # Escreva seu código abaixo
+print (mensagem[-2:])
 
-# Item g
+# Item g - Imprima o tamanho da string.
 # Escreva seu código abaixo
+print (len(mensagem))
