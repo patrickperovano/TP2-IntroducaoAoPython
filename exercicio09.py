2f6cf1a29392a7dd830144dba2c5e2fcc5de2093
Patrick Perovano <53125389+patrickperovano@users.noreply.github.com>
2024-08-18 10:14:02 +0000

    final, faltando 15 e 16

--- exercicio09.py
+++ exercicio09.py
@@ -26,15 +26,59 @@ print("vou_a_praia = ", vou_a_praia)
 # Item a
 print("\nSe eu estudar e fazer os exercícios então eu passo na prova")
 
+# TRUE
+estudar = True
+fazer_exercicios = True
+passo_na_prova = estudar and fazer_exercicios
+print ("Passo na prova = ", passo_na_prova)
+
+# FALSE
+estudar = True
+fazer_exercicios = False
+passo_na_prova = estudar and fazer_exercicios
+print ("Passo na prova = ", passo_na_prova)
 
 # Item b
 print("\nSe eu tenho um carro ou tenho uma bicicleta então eu posso viajar")
 
+# TRUE
+tenho_carro = True
+tenho_bicicleta = False
+posso_viajar = tenho_carro or tenho_bicicleta
+print ("Posso viajar = ", posso_viajar)
 
+# FALSE
+tenho_carro = False
+tenho_bicicleta = False
+posso_viajar = tenho_carro or tenho_bicicleta
+print ("Posso viajar = ", posso_viajar)
 
 # Item c
 print("\nSe estou cansado ou é tarde então eu vou dormir")
 
+# TRUE
+estou_cansado = True
+e_tarde = False
+vou_dormir = estou_cansado or e_tarde
+print ("Vou dormir = ", vou_dormir)
+
+# FALSE
+estou_cansado = False
+e_tarde = False
+vou_dormir = estou_cansado or e_tarde
+print ("Vou dormir = ", vou_dormir)
 
 # Item d
 print("\nSe eu tenho dinheiro e tempo então eu vou viajar")
+
+# TRUE
+tenho_dinheiro = True
+tenho_tempo = True
+vou_viajar = tenho_dinheiro and tenho_tempo
+print ("Vou viajar: ", vou_viajar)
+
+# FALSE
+tenho_dinheiro = True
+tenho_tempo = False
+vou_viajar = tenho_dinheiro and tenho_tempo
+print ("Vou viajar: ", vou_viajar)
